2e7a18618112ae5e89db8bc10bcd6ee145eae83c
Philipp Rost <rost.philipp@protonmail.com>
2023-03-12 20:51:36 +0100

    Code cleaned

--- nvim/after/plugin/color.lua
+++ nvim/after/plugin/color.lua
@@ -1,17 +1,12 @@
--- Hintergrund transparent
--- Standard-Farbschema definieren
---require('rose-pine').setup({
---    disable_background = true
---})
-
-function SetColorscheme(color) 
+function SetColorscheme(color)
 	-- Standard-Farbschema definieren
+
 	color = color or "kanagawa"
 	vim.cmd.colorscheme(color)
 
-	-- Hintergrund transparent ("Float" sind Fenster wie der Autovervollständigung)
+	-- Hintergrund transparent ("Float" sind Fenster wie bei der Autovervollständigung)
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#111111" })
+	--vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#ee1111" })
 end
 
 SetColorscheme()
