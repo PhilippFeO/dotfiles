df54c172a547b45fd88aaca456139a3d63b9096c
Philipp Rost <rost.philipp@protonmail.com>
2023-03-12 21:05:15 +0100

    Load saved folds

--- nvim/lua/autocommands.lua
+++ nvim/lua/autocommands.lua
@@ -5,11 +5,17 @@ vim.api.nvim_create_autocmd('BufWinLeave', {
     command = 'mkview',
     desc = 'Save folds.'
 })
+
 -- In Vimscript-Notation wesentlich kürzer
-vim.cmd([[au BufWinEnter *.* silent! loadview]])
+--vim.cmd([[au BufWinEnter *.* silent! loadview]])
+vim.api.nvim_create_autocmd('BufWinEnter', {
+    pattern = '*',
+    command = 'silent! loadview',
+    desc = "Load saved folds."
+})
 
 -- Deaktiviere Codeium beim Start
-vim.api.nvim_create_autocmd('VimEnter', {
-    command = 'Codeium Disable',
-    desc = 'Disable Codeium on startup.'
-})
+--vim.api.nvim_create_autocmd('VimEnter', {
+--    command = 'Codeium Disable',
+--    desc = 'Disable Codeium on startup.'
+--})
