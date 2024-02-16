5f03850069298ffd76d527448cf4db8989878c80
pfeo <125358469+PhilippFeO@users.noreply.github.com>
2024-02-16 07:54:43 +0100

    ✨ Feat(m-win): Switch to third workplace

--- move-windows/move-windows.sh
+++ move-windows/move-windows.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 sleep 5
-wmctrl -r Evolution -t 0
-wmctrl -r Wiki -t 3
-wmctrl -r bash -t 2
-
 wmctrl -r Cisco -t 0
+wmctrl -r Evolution -t 0
+wmctrl -r bash -t 2
+wmctrl -r Wiki -t 3
+
+wmctrl -s 2
