5364eca2aef9be885203cc7cfa136e8ee7d19c00
PhilippFeO <125358469+PhilippFeO@users.noreply.github.com>
2026-05-17 14:24:15 +0200

    🏡 17.05.2026 14:24

--- .local/share/nemo/actions/cp_fotos_diary.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# nemo action for copying fotos in $@ into ~/.tagebuch/.tmp 
-
-for img in "$@"; do
-	# notify-send "img = $img"
-	cp "$img" ~/.tagebuch/.tmp/
-done
--- .local/share/nemo/actions/mv_fotos_diary.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# nemo action for moving fotos into ~/.tagebuch/.tmp
-
-for img in "$@"; do
-	# notify-send "img = $img"
-	mv "$img" ~/.tagebuch/.tmp/
-done
